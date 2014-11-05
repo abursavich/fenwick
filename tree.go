fab80b7cdd8a1e1c0024ffda4c649869833419f0
Andrew M Bursavich <abursavich@gmail.com>
2014-11-05 15:30:46 -0800

    slice (vs struct) implementation

--- tree.go
+++ tree.go
@@ -12,59 +12,55 @@ package fenwick
 //
 // Where n is the size of the table, it requires O(n) space
 // and operations take O(log n) time.
-type Tree struct {
-	n []int
-	m int
-}
+type Tree []int
 
 // NewTree returns a new Tree with the inclusive range [min, max].
-func NewTree(min, max int) *Tree {
-	return &Tree{
-		n: make([]int, max-min+1),
-		m: min,
-	}
+func NewTree(min, max int) Tree {
+	t := make([]int, max-min+2)
+	t[0] = min
+	return t
 }
 
 // Add adds delta to index i.
-func (t *Tree) Add(i int, delta int) {
-	if i = i - t.m + 1; i < 1 {
+func (t Tree) Add(i int, delta int) {
+	if i = i - t[0] + 1; i < 1 {
 		return
 	}
-	for k := len(t.n); i <= k; i += lsb(i) {
-		t.n[i-1] += delta
+	for k := len(t); i < k; i += lsb(i) {
+		t[i] += delta
 	}
 }
 
 // Sum returns the sum of values at indices inclusively up to i.
-func (t *Tree) Sum(i int) int {
-	if i = i - t.m + 1; i > len(t.n) {
-		i = len(t.n)
+func (t Tree) Sum(i int) int {
+	if i = i - t[0] + 1; i >= len(t) {
+		i = len(t) - 1
 	}
 	n := 0
 	for ; i > 0; i = i ^ lsb(i) {
-		n += t.n[i-1]
+		n += t[i]
 	}
 	return n
 }
 
 // Range returns the sum of values in the inclusive range [i, k].
-func (t *Tree) Range(i, k int) int {
+func (t Tree) Range(i, k int) int {
 	return t.Sum(k) - t.Sum(i-1)
 }
 
 // Value returns the value at index i.
-func (t *Tree) Value(i int) int {
+func (t Tree) Value(i int) int {
 	return t.Range(i, i)
 }
 
 // Min returns the minimum index in the range.
-func (t *Tree) Min() int {
-	return t.m
+func (t Tree) Min() int {
+	return t[0]
 }
 
 // Max returns the maximum index in the range.
-func (t *Tree) Max() int {
-	return t.m + len(t.n) - 1
+func (t Tree) Max() int {
+	return t[0] + len(t) - 2
 }
 
 // lsb returns the lowest set bit.
